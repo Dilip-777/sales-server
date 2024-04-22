12b247f8d469371f03aa1ae2bbfe8d8eba9b6584
Dilip-777 <dilip7989773@gmail.com>
2024-04-22 17:20:50 +0530

    date format change

--- server/routes/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as authRouter } from "./auth";
-export { default as unitRouter } from "./unit";
-export { default as itemsRouter } from "./items";
-export { default as customerRouter } from "./customer";
-export { default as zoneRouter } from "./zone";
-export { default as categoryRouter } from "./category";
-export { default as userRouter } from "./user";
-export { default as orderRouter } from "./order";
